742e07ea9a07347778f5f522024da3956771c740
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-31 23:35:17 +0100

    app.js linked and vue.js linked tested ok

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: "#app",
+    data: {
+        todos: [{
+                task: "Watch Chelsea",
+                done: false
+            },
+            {
+                task: "Wash clothes",
+                done: true
+            },
+            {
+                task: "Watch Cartoon",
+                done: true
+            },
+        ]
+    }
+})
